5afcd1eebb55f36b482885f3937e1996eaa37bc1
Tomáš Babický <tomas.babicky@winsite.cz>
2024-11-05 17:22:04 +0100

    user profile

---
+++ app/UI/Web/User/View/ViewPresenter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\UI\Web\User\View;
+
+use App\Model\Entity\UserEntity;
+use Nette\Application\UI\Presenter;
+use Nettrine\ORM\EntityManagerDecorator;
+
+/**
+ * class ViewPresenter
+ *
+ * @package App\UI\Web\User\View
+ */
+class ViewPresenter extends Presenter
+{
+
+    public function __construct(
+        private readonly EntityManagerDecorator $em,
+    )
+    {
+    }
+
+    public function renderDefault(string $id) : void
+    {
+        $userEntity = $this->em
+            ->getRepository(UserEntity::class)
+            ->findOneBy(
+                [
+                    'id' => $id,
+                ]
+            );
+
+        if (!$userEntity) {
+            $this->error('User not found');
+        }
+
+        $this->template->userEntity = $userEntity;
+    }
+
+}
